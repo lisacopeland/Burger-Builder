7d0e1beb6385ea31c4c9595161d069d5d170c3c1
lisacopeland <lisacope@msn.com>
2018-04-14 08:24:01 -0700

    Added controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,30 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // Object.keys returns an array of the keys (in this case the ingredients passed down 
+    // in props) .map performs an operation on each element, named igKey. we then turn each 
+    // of those elements into an array with the number of elements of each ingredient, 
+    // i.e. if props.ingredient is cheese: 2 return an array with two elements and then
+    // map it and return a BurgerIngredient with a key consisting of the ingredient plus 
+    // the index, then we use reduce to find out how many ingredients we have
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }        
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,14 +2,58 @@ import React, { Component } from 'react';
  
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const Ingredient_Prices = {
+    salad: 0.5,
+    bacon: 1.7,
+    cheese: 0.85,
+    meat: 2
+}
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...props};
+    // }
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+   
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount +1;
+        // Create a constant with the ingredients from state and update it with the new 
+        // count 
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = Ingredient_Prices[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState( 
+            { ingredients: updatedIngredients, 
+              totalPrice : newPrice } );
+    }
+
+    removeIngredientHandler = (type) => {
+        
+    }
 
     render () {
         return (
             <Aux> 
-                <Burger />
-                <div>Build Controls </div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         );
     }
