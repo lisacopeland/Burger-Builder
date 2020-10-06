9a57545589575bff30efddb0ab4fd51122dd83ec
lisacopeland <lisacope@msn.com>
2020-10-05 19:39:27 -0700

    Fix syntax in error handler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -32,8 +32,8 @@ const withErrorHandler = (WrappedComponent, axios) => {
             return (
                 <Aux>
                 <Modal 
-                    show={this.state.error}>
-                    modalClosed={this.errorConfirmedHandler}
+                    show={this.state.error}
+                    modalClosed={this.errorConfirmedHandler}>
                     {this.state.error ? this.state.error.message : null}</Modal>
                 <WrappedComponent {...this.props} />
                 </Aux>
